20f50119f476a34ec4c0dc6f0b94d3e01d659d81
Tommie champion <tommyefuntoye@yahoo.com>
2022-05-01 19:44:01 +0200

    productButton

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -3,6 +3,7 @@ import "./product.css"
 import { Link } from 'react-router-dom'
 import Chart from "../../components/chart/Chart"
 import { productData } from '../../dummyData'
+import { Publish } from '@mui/icons-material'
 
 
 export default function Product() {
@@ -44,7 +45,36 @@ export default function Product() {
               </div>
             </div>
         </div>
-        <div className="productBottom"></div>
+        <div className="productBottom">
+<form className='productForm'>
+  <div className="productFormLeft">
+    <label>Product Name</label>
+    <input type="text" placeholder='Headset'/>
+    <label>In Stock</label>
+    <select name="inStock" id="idStock">
+    <option value="yes">Yes</option>
+    <option value="no">No</option>
+
+    </select>
+    <label>Active</label>
+    <select name="active" id="active">
+    <option value="yes">Yes</option>
+    <option value="no">No</option>
+
+    </select>
+  </div>
+  <div className="productFormRight">
+<div className="productUpload">
+  <img src="https://i5.walmartimages.com/asr/fad4ba0a-3036-4a5b-b1f0-cc9dcff587d3.f8b80685300f691be2201720f5e7a498.jpeg" alt="" className="productUploadImg" />
+<label for="file">
+<Publish></Publish>
+</label>
+<input type="file" id="file" style={{display:"none"}} ></input>
+</div>
+<button className="productButton">Update</button>
+  </div>
+</form>
+        </div>
       </div>
   )
 }
